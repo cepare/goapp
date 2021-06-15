2af686bcdf156431716d32ac8f9a50edbae13fa2
cepare366 <cepare366@gmail.com>
2021-06-15 15:04:50 -0500

    Funcionalidad de edicion de páginas
    Se crea la funcionalidad de edición de páginas la cual permite
    editar una pagina exitente o crea una nueva si es que no existe.

--- goapp.go
+++ goapp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 type Page struct {
@@ -16,16 +17,66 @@ func (p *Page) save() error {
 	return err
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := "./data/" + title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
 	page := &Page{Title: title, Body: body}
-	return page
+	return page, err
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+
+		fmt.Fprintf(w, "<h1>%s</h1>", err)
+	}
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	page, err := loadPage(title)
+	if err != nil {
+		page = &Page{Title: title}
+	}
+	fmt.Fprintf(w, `
+		<html>
+		<head>
+			<title>%s</title>
+		</head>
+		<body>
+			<h1>%s</h1>
+			<form method="POST" action="/save/%s">
+			<textarea name="body">%s</textarea>
+			<button>Guardar</button>
+			</form>
+		</body>
+		</html>
+	`, page.Title, page.Title, page.Title, page.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	page := &Page{Title: title, Body: []byte(body)}
+	page.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Bienvenidos</h1>")
 }
 
 func main() {
-	/*page := &Page{Title: "primer", Body: []byte("Nuestra primera pagina")}
-	page.save()*/
-	page := loadPage("primer")
-	fmt.Println(page.Title, string(page.Body))
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	fmt.Println("Ingrese a http://localhost:8080/view/")
+	http.ListenAndServe(":8080", nil)
+
 }
